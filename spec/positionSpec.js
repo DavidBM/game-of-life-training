a9d10f70680264a5a9caf67291d62fa97620981f
pc3 <webbonet@gmail.com>
2017-07-26 15:47:36 +0200

    Add get neightbours

--- spec/positionSpec.js
+++ spec/positionSpec.js
@@ -4,4 +4,17 @@ describe('Position', function() {
 
 		expect(position.getHash()).toBe('2,4');
 	});
+
+	it('should return the neighbours hashes', function() {
+		var position = new Position('2,4');
+
+		var neighbours = ['1,3', '1,4', '1,5', '2,3', '2,5', '3,3', '3,4', '3,5'];
+
+		var neighboursHashes = position.getNeighboursHash();
+
+		var same = neighbours.filter(neig => neighboursHashes.includes(neig));
+
+		expect(neighboursHashes.length).toBe(neighbours.length);
+		expect(same.length).toBe(neighbours.length);
+	});
 });
--- src/position.js
+++ src/position.js
@@ -1,9 +1,27 @@
 class Position {
 	constructor(hash) {
-		this.coordinates = hash.split(',');
+		this.coordinates = hash.split(',').map(n => Number.parseInt(n));
 	}
 
 	getHash(){
 		return this.coordinates.join(',');
+	}
+
+	getNeighboursHash(){
+		var x = this.coordinates[0];
+		var y = this.coordinates[1];
+
+		return [
+			[x-1, y-1].join(','),
+			[x  , y-1].join(','),
+			[x+1, y-1].join(','),
+
+			[x+1, y].join(','),
+			[x-1, y].join(','),
+
+			[x-1, y+1].join(','),
+			[x  , y+1].join(','),
+			[x+1, y+1].join(','),
+		]
 	}
 }
