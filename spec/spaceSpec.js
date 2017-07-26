3ba4d4d56fd1f9f17dea365a41d3e60632a8408a
pc3 <webbonet@gmail.com>
2017-07-26 16:59:42 +0200

    getAlivePositions

--- spec/spaceSpec.js
+++ spec/spaceSpec.js
@@ -13,4 +13,13 @@ describe('Space', function() {
 
 		expect(space.getCell(position)).toBe(cell);
 	});
+
+	it('should return the computable cells', function() {
+		space.addCell(new Cell(true), new Position('2,3'));
+		space.addCell(new Cell(true), new Position('2,4'));
+		space.addCell(new Cell(false), new Position('3,3'));
+		space.addCell(new Cell(true), new Position('13,34'));
+
+		expect(space.getAlivePositions().length).toBe(3);
+	});
 });
--- src/space.js
+++ src/space.js
@@ -4,10 +4,19 @@ class Space  {
 	}
 
 	addCell(cell, position){
-		this.positions[position.getHash()] = cell;
+		if(cell.getState())
+			this.positions[position.getHash()] = cell;
 	}
 
 	getCell(position){
+		var cell = this.positions[position.getHash()];
+		if(!cell)
+			return new Cell(false);
+
 		return this.positions[position.getHash()];
+	}
+
+	getAlivePositions(){
+		return Object.keys(this.positions).map(hash => this.positions[hash]);
 	}
 }
