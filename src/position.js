17d4746a06b8a1bdd7eff15d86428fd0ee6866b4
pc3 <webbonet@gmail.com>
2017-07-26 16:48:33 +0200

    N dimensional

--- src/position.js
+++ src/position.js
@@ -7,21 +7,51 @@ class Position {
 		return this.coordinates.join(',');
 	}
 
+	static fromCoordinates(coordinates) {
+		return new Position(coordinates.join(','));
+	}
+
 	getNeighboursHash(){
-		var x = this.coordinates[0];
-		var y = this.coordinates[1];
+		var positions = this.calculateNeighbours(this.coordinates, 0);
 
-		return [
-			[x-1, y-1].join(','),
-			[x  , y-1].join(','),
-			[x+1, y-1].join(','),
+		return positions
+		.map(coord => Position.fromCoordinates(coord).getHash())
+		//removes the actual position
+		.filter(coord => coord !== this.getHash());
+	}
 
-			[x+1, y].join(','),
-			[x-1, y].join(','),
+	calculateNeighbours(coordinates, index){
+		var result = [];
 
-			[x-1, y+1].join(','),
-			[x  , y+1].join(','),
-			[x+1, y+1].join(','),
-		]
+		if(index >= coordinates.length - 1){
+			var coord1 = this.cloneCoordinates(coordinates);
+			coord1[index] -= 1;
+			result.push(coord1);
+
+			result.push(this.cloneCoordinates(coordinates));
+
+			var coord2 = this.cloneCoordinates(coordinates);
+			coord2[index] += 1;
+			result.push(coord2);
+
+			return result;
+		}
+
+		var coordPlus = this.cloneCoordinates(coordinates);
+		coordPlus[index] += 1;
+		var result1 = this.calculateNeighbours(coordPlus, index + 1);
+
+		var coordLess = this.cloneCoordinates(coordinates);
+		coordLess[index] -= 1;
+		var result2 = this.calculateNeighbours(coordLess, index + 1);
+
+		var coord = this.cloneCoordinates(coordinates);
+		var result3 = this.calculateNeighbours(coord, index + 1);
+
+		return result.concat(result1).concat(result2).concat(result3);
+	}
+
+	cloneCoordinates(coordinates){
+		return coordinates.slice(0);
 	}
 }
